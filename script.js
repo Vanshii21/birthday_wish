a632384988382c08b053e0f1975e502a01e11b34
Vanshika0421 <89936851+Vanshika0421@users.noreply.github.com>
2022-01-02 14:50:31 +0530

    project completed

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "Vanshu" && b=="21-02-2002")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
